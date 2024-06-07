35eb0dd1d10532cc32262512100953abcfa45b37
Himanshuchoudhary311999 <himanshu.datascience@yahoo.com>
2024-06-07 17:15:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
+  console.log(`Server is running on port ${port}`);
 });
 
